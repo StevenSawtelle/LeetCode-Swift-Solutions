babf605f002b16bc7d4b41d307c95b0ae8abaedb
Steven Sawtelle <steven.sawtelle@gmail.com>
2017-06-14 23:05:45 -0700

    Solution for 1. Two Sum

---
+++ twoSum.swift
@@ -0,0 +1,25 @@
+/*
+
+1. Two Sum
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        // Dictionary maps target to index
+        var targetToIndex = [Int: Int]()
+        
+        for currentIndex in 0..<nums.count{
+            // Return both indices if target is met
+            if let foundIndex = targetToIndex[nums[currentIndex]]{
+                return [foundIndex, currentIndex]
+            }
+            // Otherwise map the target to the result of the value to index
+            targetToIndex[target - nums[currentIndex]] = currentIndex
+        }
+        // Default return (even though the assumption is there will always be something)
+        return [-1,-1]
+    }
+}
